6a34fd337e3568ebb1d5e9d1c073fe6a659ce48b
vadimcimbalyk <vadymonua@gmail.com>
2022-04-26 12:45:35 +0300

    fix catalog

--- js/script.js
+++ js/script.js
@@ -1,33 +1,3 @@
-$('.catalog').slick({
-    slideToShow: 6,
-    slidesToScroll: 3,
-    dots: true,
-    rows: 2,
-    slidesPerRow: 3,
-    arrows: true,
-    nextArrow: '<button type="button" class="slick-prev"></button>',
-    prevArrow: '<button type="button" class="slick-next "></button>',
-    responsive: [{
-        breakpoint: 1024,
-        settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            slidesPerRow: 2,
-            rows: 2,
-            // infinite: true,
-            // dots: true
-        }
-    }, {
-        breakpoint: 768,
-        settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            slidesPerRow: 1,
-            rows: 1,
-
-        }
-    }]
-});
 
 const formInput = document.querySelectorAll('.form-control')
 
